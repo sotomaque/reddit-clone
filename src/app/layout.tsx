8baaeb7db2490539e08362a318aa3df160d7a726
sotomaque <sotomaque@gmail.com>
2023-07-06 08:04:38 -1000

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
+import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
 
 export const metadata = {
   title: 'Reddit Clone',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 };
+
+// Custom Font
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -11,7 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}
+    >
       <body>{children}</body>
     </html>
   );
